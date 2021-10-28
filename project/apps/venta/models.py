e94459d3708913eb1083676bd5f51748c1e7c6f5
Pacho04 <davidsanchezmotran@gmail.com>
2021-10-28 16:10:18 -0300

    ADD la app de Venta con los modelos de Product, Order, OrderDetail. migraciones

---
+++ project/apps/venta/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+class Product(models.Model):
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+    date_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.date_time
+
+
+class OrderDetail(models.Model):
+    class Meta:
+        verbose_name = 'Order Detail'
+        verbose_name_plural = 'Orders Detail'
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cuantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.order} -> {self.product}'
